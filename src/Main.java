dcc9be593baeef3df7cff09fd81147819f611b62
trieu <longgiang9999.gl@gmail.com>
2023-03-26 00:34:04 +0700

    tim max trong mang su dung for de duyet

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int size;
+        int array[];
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nhap do dai cua mang: ");
+        size = scanner.nextInt();
+        array = new int[size];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = scanner.nextInt();
+        }
+        for (int j = 0; j < array.length; j++) {
+            System.out.println(array[j]);
+        }
+        int max = array[0];
+        int index = 0;
+        for (int i = 0; i <array.length ; i++) {
+                if(array[i] > max){
+                    max =  array[i];
+                    index = i +1 ;
+                }
+        }
+        System.out.println("gia tri lon nhat  cua mang la: " + max + " vi tri thu: " + index);
     }
 }
